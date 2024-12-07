ec58e217b48985327e7a832a2ec0ae6501ade284
Icarucci <163043419+Icarucci@users.noreply.github.com>
2024-12-07 20:04:44 -0300

    Handlebar

--- Primer_pre_entrega/src/app.js
+++ Primer_pre_entrega/src/app.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import productsRouter from './routes/products.routes.js';
 import { cartsRouter } from './routes/carts.routes.js';
+import { Server } from 'socket.io';
+import { create } from 'express-handlebars';
 
 const app = express();
+const hbs = create();
 
 //Preparar config 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/views'));
 
 //Routers
 app.get('ping' , (req, res) => {
@@ -18,6 +24,28 @@ app.use('/api/cart' , cartsRouter);
 
 
 const SERVER_PORT = 8080;
-app.listen(SERVER_PORT, () => {
-    console.log("Servidor escuchando por el puerto: "+ SERVER_PORT);
+//app.listen(SERVER_PORT, () => {
+//    console.log("Servidor escuchando por el puerto: "+ SERVER_PORT);
+//});
+
+const server = app.listen(SERVER_PORT,() => {
+    console.log(`Servidor corriendo en puerto: ${SERVER_PORT}`);
+});
+
+const io = new Server (server);
+
+//Websocket
+let messages = [];
+
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado', socked.id);
+
+    socket.on('mensaje', (data) => {
+        console-console.log("Mensaje recibido; ", data);
+        messages.push(data);
+        socket.emit('Respuesta', message);
+    });
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado', socket.id);
+    });
 });
